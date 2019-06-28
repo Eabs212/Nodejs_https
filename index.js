e2824d1aeec1a44160f99b3bdde3be00ef2b5940
Andres Marmol <andresmarmolm@gmail.com>
2019-06-28 11:20:22 -0400

    Enviar archivo

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const cp = require('child_process')
 const express = require('express')
 const app = express()
 const https = require('https')
 const fs = require('fs')
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("IT'S WORKING!")
+
+
+app.post('/', (req, res) => {
+    let writeable = fs.createWriteStream('./test')
+    req.pipe(writeable);
+    req.on('end',() => {
+        cp.exec('code test', (err, stdout) => {
+            if(err) throw err;
+            console.log(stdout)
+        })
+        res.end('File received\n')
+    })
+    
 })
 
 const httpsOptions = {
